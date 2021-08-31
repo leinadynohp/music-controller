b966e4016d158f66874d08862863e4c18577fa81
Leinad Kim <leinadynohp@gmail.com>
2021-09-01 00:07:18 +0900

    skipping songs

--- frontend/src/components/MusicPlayer.js
+++ frontend/src/components/MusicPlayer.js
@@ -9,6 +9,13 @@ export default class MusicPlayer extends Component {
         super (props);
     }
 
+    skipSong () {
+        const requestOptions = {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+        };
+        fetch('/spotify/skip', requestOptions)
+    }
     pauseSong() {
         const requestOptions = {
             method: 'PUT',
@@ -44,7 +51,8 @@ export default class MusicPlayer extends Component {
                             <IconButton onClick={ () => {this.props.is_playing ? this.pauseSong() : this.playSong()}}>
                                 {this.props.is_playing ? <PauseIcon /> : <PlayArrowIcon />}
                             </IconButton>
-                            <IconButton>
+                            <IconButton onClick={ () => this.skipSong()}>
+                                {this.props.votes} / {this.props.votes_required}
                                 <SkipNextIcon />
                             </IconButton>
                         </div>
